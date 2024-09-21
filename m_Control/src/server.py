f7336b67594c4c81fe01e2e0a0d224ae19db9b27
Mup3nda <mupendadidier@gmail.com>
2024-09-21 12:00:56 +0200

    legge til en fil

---
+++ m_Control/src/server.py
@@ -0,0 +1,13 @@
+import asyncio
+import websockets
+
+async def handle_client(websocket, path):
+    while True:
+        data = await websocket.recv()
+        x, y, z = data.split(',')
+        print(f"Received coordinates: x={x}, y={y}, z={z}")
+
+start_server = websockets.serve(handle_client, "0.0.0.0", 8765)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
