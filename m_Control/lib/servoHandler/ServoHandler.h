739f03f4e583395dda2309d8c311a4b883eaa924
AbdiYus <yusabdirahman@gmail.com>
2024-10-09 08:51:53 +0200

    servo control

--- m_Control/lib/servoHandler/ServoHandler.h
+++ m_Control/lib/servoHandler/ServoHandler.h
@@ -6,24 +6,30 @@
 
 class ServoHandler {
 public:
-    ServoHandler();
+    ServoHandler(const int defaultpos[]);
+    ~ServoHandler();
     void setupServos();
-    void servoMove(std::vector<int> stepVector);
-    void servoSetPosition(std::vector<int> wantedPos);
+    void servoMove(std::vector<int> &stepVector);
+    void servoSetPosition(std::vector<int> &wantedPos, char debug[]);
     static const int SERVO_DELAY = 1000; // Delay in milliseconds
+    void test(); 
 
 private:
-    Adafruit_PWMServoDriver pwm;
+    Adafruit_PWMServoDriver* pwm;
     static const int SERVOS = 3;
-    static const int SERVO_MIN = 100;
-    static const int SERVO_MAX = 600;
+    static const int SERVO_MIN = 125; // 1ms -> 0 degree 
+    static const int SERVO_MAX = 625; // 2ms -> 180 degree
+    unsigned long previousTime = 0; 
+    unsigned long interval = 15; 
 
-    int servoPins[SERVOS];
+    
+
     const int servoMinMax[3][2];
-    int defaultServoPositions[SERVOS];
-    int servoPositions[SERVOS];
+    std::vector<int> servoPins;
+    std::vector<int> defaultServoPositions;
+    std::vector<int> servoPositions;
 
-    std::vector<int> checkParams(std::vector<int> targetPosition);
+    std::vector<int> checkParams(std::vector<int> &targetPosition);
     int degreesToPulseWidth(int degrees); // New function to convert degrees to pulse width
 };
 
